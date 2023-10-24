d7bf3305cc731174046576854770fc16bfbe625d
“MAILLAgit config --global user.email “mailladrumond@gmail.com <mailladrumond@gmail.com>
2023-10-24 04:41:31 -0300

    alteracoes midia sociais

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
-    img.setAttribute('src', './assets/avatar_light.png')
+    img.setAttribute("src", "./assets/Avatar_light.png")
   } else {
     //se tiver sem light mode,manter a imagem normal
-    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute("src", "./assets/Avatar.png")
   }
 }
